bdf1655eaded823227f10f00ec2b20c07c617c49
Tim Goodwin <tdgoodwin1983@gmail.com>
2026-08-15 21:57:02 -0400

    Update fan schema for ESPHome 2026.7
    Removed CONF_RESTORE_MODE from configuration schema and updated fan schema extension.

--- components/ble_adv_controller/fan/__init__.py
+++ components/ble_adv_controller/fan/__init__.py
@@ -4,7 +4,6 @@ from esphome.components import fan
 
 from esphome.const import (
     CONF_OUTPUT_ID,
-    CONF_RESTORE_MODE,
 )
 
 from .. import (
@@ -20,23 +19,21 @@ from ..const import (
     CONF_BLE_ADV_OSCILLATION_SUPPORTED,
     CONF_BLE_ADV_FORCED_REFRESH_ON_START,
 )
-
 BleAdvFan = bleadvcontroller_ns.class_('BleAdvFan', fan.Fan, BleAdvEntity)
-
 CONFIG_SCHEMA = cv.All(
-    fan.FAN_SCHEMA.extend(
+    fan.fan_schema(
+        BleAdvFan,
+        default_restore_mode="RESTORE_DEFAULT_OFF",
+    ).extend(
         {
             cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(BleAdvFan),
             cv.Optional(CONF_BLE_ADV_SPEED_COUNT, default=6): cv.one_of(0,3,6),
             cv.Optional(CONF_BLE_ADV_DIRECTION_SUPPORTED, default=True): cv.boolean,
             cv.Optional(CONF_BLE_ADV_OSCILLATION_SUPPORTED, default=False): cv.boolean,
             cv.Optional(CONF_BLE_ADV_FORCED_REFRESH_ON_START, default=True): cv.boolean,
-            # override default value for restore mode, to always restore as it was if possible
-            cv.Optional(CONF_RESTORE_MODE, default="RESTORE_DEFAULT_OFF"): cv.enum(fan.RESTORE_MODES, upper=True, space="_"),
         }
     ).extend(ENTITY_BASE_CONFIG_SCHEMA),
 )
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_OUTPUT_ID])
